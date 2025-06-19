1585d481f67c3768b05febdc10f08d90f49c7231
carloshenriqueok <ifcarloshenrique@gmail.com>
2025-06-19 17:26:21 -0300

    FEAT: Create views and route of product #27

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from core.views import CustomerViewSet
+from core.views import CustomerViewSet, ProductViewSet
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet)
+router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
