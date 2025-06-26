abca14a03246ed06a0261dc17502e2a743eca03b
carloshenriqueok <ifcarloshenrique@gmail.com>
2025-06-25 22:06:32 -0300

    FEAT: Create views and route of order #32

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from core.views import CustomerViewSet, ProductViewSet, FuncionarioViewSet, MateriaPrimaViewSet, StockViewSet
+from core.views import CustomerViewSet, ProductViewSet, FuncionarioViewSet, MateriaPrimaViewSet, StockViewSet, OrderViewSet
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet)
 router.register(r'products', ProductViewSet)
 router.register(r'employees', FuncionarioViewSet)
 router.register(r'rawMaterials', MateriaPrimaViewSet)
 router.register(r'stocks', StockViewSet)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
